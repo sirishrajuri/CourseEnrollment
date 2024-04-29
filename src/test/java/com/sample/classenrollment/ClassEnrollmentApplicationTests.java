7dad32184f17a6676fb42dfec3ba057c004a2324
Sirish Rajuri <sirish.rajuri.sr@gmail.com>
2024-04-28 20:03:51 -0400

    base setup

---
+++ src/test/java/com/sample/classenrollment/ClassEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.classenrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassEnrollmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
